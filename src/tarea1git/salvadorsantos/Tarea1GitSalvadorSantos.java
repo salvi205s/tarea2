7865fbb661c0433da9d3141adffc746dcd9984ad
Salva <salvi205s@gmail.com>
2018-12-19 14:21:57 +0100

    Añade mensaje de bienvenida

--- src/tarea1git/salvadorsantos/Tarea1GitSalvadorSantos.java
+++ src/tarea1git/salvadorsantos/Tarea1GitSalvadorSantos.java
@@ -15,7 +15,9 @@ public class Tarea1GitSalvadorSantos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        System.out.println("Aplicacion de Salvador Santos");
+
     }
     
 }
